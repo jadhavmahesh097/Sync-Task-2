877293d27f8398d075b0a2bda5c793b18b6af0d8
Mahesh Jadhav <130351585+jadhavmahesh097@users.noreply.github.com>
2023-04-14 10:28:16 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,112 @@
+let quiz_rules = [
+    "You'll get marks on the basic of your correct answers.",
+    "You have to solve given Question within a time limit given to you.",
+    "Once you select any anwer. It can't be undone",
+    
+];
+
+
+let quiz_questions = [
+    // question 
+    {
+        question: "Who is the prime minisrer of India?",
+
+        options : [
+            " Rahul Gandhi",
+            " Amit Shah",
+            " Narendra Modi",
+            " Sonia Gandhi",
+        ],
+
+        // correct answer
+        answer: 3,
+    },
+    
+    {
+        question: "Who is chief minister of Maharastra ?",
+    
+        options: [
+            "Devendra Fadanvis",
+            "Eknath Shinde",
+            "Ajit Pawar",
+            "Uddhav Thakrey",
+        ],
+        answer: 2,
+    },
+    {
+        question: "How many days in August?",
+    
+        options: [
+            "31",
+            "28",
+            "29",
+            "30",
+        ],
+        answer: 1,
+
+    },
+    {
+        question: "How many meters in 1 kilometer?",
+    
+        options: [
+            "500",
+            "2000",
+            "1000",
+            "800",
+        ],
+        answer: 3,
+    },
+    {
+        question: " In which year India celebrated its first Republic day?",
+    
+        options: [
+            "1947",
+            "1950",
+            "1948",
+            "1960",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Which is the largest state of India by Area ?",
+    
+        options: [
+            "Maharastra",
+            "Uttar Pradesh",
+            "Rajasthan",
+            "Madhya Pradesh",
+            
+        ],
+
+        answer: 3,
+    },
+    {
+
+        question: "Which Indian state has a separate constitution?",
+    
+        options: [
+            "Goa",
+            "Delhi",
+            "Jammu and Kashmir",
+            "Deev and Daman",
+        ],
+        
+        answer: 3,
+
+    },
+    {
+        
+        question: "The Indian Institute of Science is located in which Indian city ?",
+    
+        options: [
+            "Mumbai",
+            "Banglore",
+            "Chennai",
+            "Hyderabad",
+        ],
+        
+        answer: 2,
+    
+    },
+    
+];
